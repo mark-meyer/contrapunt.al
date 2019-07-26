73316318a0b25432a6bb502b26f7b6f6c7b1d4b5
Mark Meyer <mark@photo-mark.com>
2019-07-26 14:09:32 -0800

    redraw on resize and add media queries

--- src/index.js
+++ src/index.js
@@ -2,22 +2,25 @@ import './style.css';
 import paper, { view, Path, Group, Point, Symbol } from 'paper'
 import {makeGraph} from './graph.js'
 
-var ROWS = 35
-var COLS = 35
+const MAX_ROWS = 35
+const MAX_COLS = 35
+const REFESH_RATE = 60
 
 function draw(ctx){ 
-    var h_space = ctx.width / COLS
-    var v_space = ctx.height / ROWS
+    var h_space = ctx.width / ctx.cols
+    var v_space = ctx.height / ctx.rows
     var g = ctx.g
-    ctx.symbol.place(new Point(h_space/2, v_space/2 ));
+    if (ctx.nodes_drawn == false){
+        ctx.symbol.place(new Point(h_space/2, v_space/2 ));
+    }
     for (var i = 1; i < g.length; i++){
         var to = g[i].label
         var from = g[i].edges[0].node
-        var t_col = to  % COLS * h_space + h_space/2
-        var t_row = Math.floor(to / ROWS) * v_space + v_space/2
+        var t_col = to  % ctx.cols * h_space + h_space/2
+        var t_row = Math.floor(to / ctx.cols) * v_space + v_space/2
 
-        var f_col = from  % COLS * h_space +  h_space/2
-        var f_row = Math.floor(from / ROWS) * v_space + v_space/2
+        var f_col = from  % ctx.cols * h_space +  h_space/2
+        var f_row = Math.floor(from / ctx.cols) * v_space + v_space/2
 
         var p = new Path();
         
@@ -41,33 +44,43 @@ document.addEventListener("DOMContentLoaded",() => {
     }
     var width = paper.project.view.size.width
     var height = paper.project.view.size.height
-    var node_icon = new Path.Circle(new Point(10, 10), 4);
-    var symbol = new Symbol(node_icon);
-    var links = new Group()
-    
- 
+    console.log(width, height)
+    let rows = Math.min(MAX_ROWS, Math.floor(height/25))
+    let cols = Math.min(MAX_COLS, Math.floor(width/25))
+
     var ctx = {
-        g: makeGraph(ROWS, COLS),
+        g: makeGraph(rows, cols),
         width,
         height,
+        rows,
+        cols,
         nodes_drawn: false,
-        symbol,
-        links
+        symbol:  new Symbol( new Path.Circle(new Point(10, 10), 4)),
+        links: new Group()
     }
-
     draw(ctx)
 
     view.onFrame = function (event) {
-        node_icon.fillColor =  "#ffffff"
-        if(event.count >= 120 && event.count % 120 == 0){
-            node_icon.fillColor =  "#fafafa"
-            ctx.g = makeGraph(ROWS, COLS)
-            links.removeChildren()
+        if(event.count >= REFESH_RATE && event.count % REFESH_RATE == 0){
+            ctx.g = makeGraph(ctx.rows, ctx.cols)
+            ctx.links.removeChildren()
             draw(ctx)
         }
     }
+
+    view.onResize = function(event) {
+            paper.project.activeLayer.removeChildren()
+            ctx.links  = new Group()
+            ctx.height = paper.project.view.size.height
+            ctx.width  = paper.project.view.size.width
+            ctx.rows   = Math.min(MAX_ROWS, Math.floor(ctx.height/25)),
+            ctx.cols   = Math.min(MAX_COLS, Math.floor(ctx.width/25)),
+            ctx.g = makeGraph(ctx.rows, ctx.cols),
+            ctx.nodes_drawn = false
+            draw(ctx)        
+    }
 })
-//
+
 
 
 
